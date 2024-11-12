29478da182d413c1dbbf69686c7239f9902c8e6c
Johnny Tran <tranjohnny917@gmail.com>
2024-11-12 10:55:27 -0500

    Add project files.

---
+++ EmployeePay/frmMain.Designer.cs
@@ -0,0 +1,148 @@
+﻿
+namespace EmployeePay
+{
+    partial class frmTitleBar
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblHourlyRate = new System.Windows.Forms.Label();
+            this.lblHoursWorked = new System.Windows.Forms.Label();
+            this.lblDisplay = new System.Windows.Forms.Label();
+            this.bntClose = new System.Windows.Forms.Button();
+            this.txtHourlyRate = new System.Windows.Forms.TextBox();
+            this.txtHoursWorked = new System.Windows.Forms.TextBox();
+            this.btnCalculatePay = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblHourlyRate
+            // 
+            this.lblHourlyRate.AutoSize = true;
+            this.lblHourlyRate.Location = new System.Drawing.Point(78, 106);
+            this.lblHourlyRate.Name = "lblHourlyRate";
+            this.lblHourlyRate.Size = new System.Drawing.Size(91, 13);
+            this.lblHourlyRate.TabIndex = 0;
+            this.lblHourlyRate.Text = "Enter Hourly Rate";
+            // 
+            // lblHoursWorked
+            // 
+            this.lblHoursWorked.AutoSize = true;
+            this.lblHoursWorked.Location = new System.Drawing.Point(78, 150);
+            this.lblHoursWorked.Name = "lblHoursWorked";
+            this.lblHoursWorked.Size = new System.Drawing.Size(104, 13);
+            this.lblHoursWorked.TabIndex = 1;
+            this.lblHoursWorked.Text = "Enter Hours Worked";
+            // 
+            // lblDisplay
+            // 
+            this.lblDisplay.AutoSize = true;
+            this.lblDisplay.ForeColor = System.Drawing.Color.Red;
+            this.lblDisplay.Location = new System.Drawing.Point(363, 106);
+            this.lblDisplay.Name = "lblDisplay";
+            this.lblDisplay.Size = new System.Drawing.Size(51, 13);
+            this.lblDisplay.TabIndex = 2;
+            this.lblDisplay.Text = "lblDisplay";
+            this.lblDisplay.Visible = false;
+            this.lblDisplay.Click += new System.EventHandler(this.lblDisplay_Click);
+            // 
+            // bntClose
+            // 
+            this.bntClose.Location = new System.Drawing.Point(80, 238);
+            this.bntClose.Name = "bntClose";
+            this.bntClose.Size = new System.Drawing.Size(80, 40);
+            this.bntClose.TabIndex = 3;
+            this.bntClose.Text = "Close";
+            this.bntClose.UseVisualStyleBackColor = true;
+            this.bntClose.Click += new System.EventHandler(this.bntClose_Click);
+            // 
+            // txtHourlyRate
+            // 
+            this.txtHourlyRate.Location = new System.Drawing.Point(216, 106);
+            this.txtHourlyRate.Name = "txtHourlyRate";
+            this.txtHourlyRate.Size = new System.Drawing.Size(100, 20);
+            this.txtHourlyRate.TabIndex = 4;
+            // 
+            // txtHoursWorked
+            // 
+            this.txtHoursWorked.Location = new System.Drawing.Point(216, 147);
+            this.txtHoursWorked.Name = "txtHoursWorked";
+            this.txtHoursWorked.Size = new System.Drawing.Size(100, 20);
+            this.txtHoursWorked.TabIndex = 5;
+            // 
+            // btnCalculatePay
+            // 
+            this.btnCalculatePay.Location = new System.Drawing.Point(252, 238);
+            this.btnCalculatePay.Name = "btnCalculatePay";
+            this.btnCalculatePay.Size = new System.Drawing.Size(80, 40);
+            this.btnCalculatePay.TabIndex = 6;
+            this.btnCalculatePay.Text = "Calculate Pay";
+            this.btnCalculatePay.UseVisualStyleBackColor = true;
+            this.btnCalculatePay.Click += new System.EventHandler(this.btnCalculatePay_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(166, 238);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(80, 40);
+            this.btnClear.TabIndex = 7;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // frmTitleBar
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnCalculatePay);
+            this.Controls.Add(this.txtHoursWorked);
+            this.Controls.Add(this.txtHourlyRate);
+            this.Controls.Add(this.bntClose);
+            this.Controls.Add(this.lblDisplay);
+            this.Controls.Add(this.lblHoursWorked);
+            this.Controls.Add(this.lblHourlyRate);
+            this.Name = "frmTitleBar";
+            this.Text = "Johnny Tran - Employee Pay Calculator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblHourlyRate;
+        private System.Windows.Forms.Label lblHoursWorked;
+        private System.Windows.Forms.Label lblDisplay;
+        private System.Windows.Forms.Button bntClose;
+        private System.Windows.Forms.TextBox txtHourlyRate;
+        private System.Windows.Forms.TextBox txtHoursWorked;
+        private System.Windows.Forms.Button btnCalculatePay;
+        private System.Windows.Forms.Button btnClear;
+    }
+}
+
---
+++ EmployeePay/frmMain.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace EmployeePay
+{
+    public partial class frmTitleBar : Form
+    {
+        public frmTitleBar()
+        {
+            InitializeComponent();
+        }
+
+        private void bntClose_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            txtHoursWorked.Clear();
+            txtHourlyRate.Clear();
+            lblDisplay.Text = "";
+            
+        }
+
+        private void btnCalculatePay_Click(object sender, EventArgs e)
+        {
+            double StateIncomeTaxDeduction = 0.035;
+            double FederalIncomeTaxDeduction = 0.15;
+            double SocialSecurity = 0.062;
+            double Medicare = 0.029;
+            double NetPay;
+
+            double Hours, Rate;
+
+            // Try parsing txtHoursWorked.Text
+            if (!double.TryParse(txtHoursWorked.Text, out Hours) || Hours < 0)
+            {
+                lblDisplay.Text = "Please enter valid numbers for hours worked and hourly rate.";
+                lblDisplay.Visible = true;
+                return; 
+            }
+
+            // Try parsing txtHourlyRate.Text
+            if (!double.TryParse(txtHourlyRate.Text, out Rate))
+            {
+                lblDisplay.Text = "Please enter valid numbers for hourly rate and hours worked.";
+                lblDisplay.Visible = true;
+                return; 
+            }
+
+            double GrossPay = Hours * Rate;
+            GrossPay = Math.Round(GrossPay, 2);
+
+            //TaxDeduction
+            StateIncomeTaxDeduction *= GrossPay;
+            StateIncomeTaxDeduction =  Math.Round(StateIncomeTaxDeduction, 2);
+            FederalIncomeTaxDeduction *= GrossPay;
+            FederalIncomeTaxDeduction = Math.Round(FederalIncomeTaxDeduction, 2);
+
+
+            // FICA deductions
+            SocialSecurity *= GrossPay;
+            SocialSecurity = Math.Round(SocialSecurity, 2);
+            Medicare *= GrossPay;
+            Medicare = Math.Round(Medicare, 2);
+
+            NetPay = Math.Round(GrossPay - StateIncomeTaxDeduction - FederalIncomeTaxDeduction
+                    - SocialSecurity - Medicare,2);
+
+
+
+            lblDisplay.Text = "The gross pay is " + GrossPay.ToString() +
+                              ". \nThe State Income Tax deduction is " + StateIncomeTaxDeduction.ToString() +
+                              ". \nThe Federal Income tax deduction is " + FederalIncomeTaxDeduction.ToString() +
+                              ". \nThe Social Security is " + SocialSecurity.ToString() +
+                              ". \nThe Medicare is " + Medicare.ToString() + 
+                              ". \nThe net pay is " + NetPay.ToString();
+
+            lblDisplay.Visible = true;
+
+        }
+
+        private void lblDisplay_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
